9d666830e100dac6c4167569ea7c4cb08e8b3c68
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-03-13 13:56:23 +0100

    Added question and answers to first card

---
+++ script/questions.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        "question": "Welche Hunderasse ist für ihre blau-schwarze Zunge bekannt?",
+        "answer_1": "Labrador Retriever",
+        "answer_2": "Chow-Chow",
+        "answer_3": "Jack Russel Terrier",
+        "answer_4": "Beagle",
+        "right_answer": 2
+    },
+
+    {
+        "question": "Welche Hunderasse ist dafür bekannt, eine besonders gute Spürnase zu haben?",
+        "answer_1": "Bloodhound",
+        "answer_2": "Boxer",
+        "answer_3": "Whippet",
+        "answer_4": "Berner Sennenhund",
+        "right_answer": 1
+    },
+
+    {
+        "question": "Welche Hunderasse gilt als die kleinste der Welt?",
+        "answer_1": "Dalamatiner",
+        "answer_2": "Labradoodle",
+        "answer_3": "Mops",
+        "answer_4": "Chihuahua",
+        "right_answer": 4
+    },
+
+    {
+        "question": `Welcher Hund wird oft als "die älteste Hunderasse" bezeichnet?`,
+        "answer_1": "Australian Shepherd",
+        "answer_2": "Collie",
+        "answer_3": "Basenij",
+        "answer_4": "Boxer",
+        "right_answer": 3
+    },
+
+    {
+        "question": "Welche britische Hunderasse ist für ihre kurzen Beine und ihren treuen Charakter bekannt?",
+        "answer_1": "Shetland Sheepdog",
+        "answer_2": "Pembroke Welsh Corgi",
+        "answer_3": "Border Terrier",
+        "answer_4": "Yorkshire Terrier",
+        "right_answer": 2
+    }
+];
---
+++ script/script.js
@@ -0,0 +1,15 @@
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('allQuestions').innerHTML = questions.length;
+    showQuestion();
+}
+
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('questionText').innerHTML = question['question'];
+    for (let i = 1; i <= 4; i++) {
+        document.getElementById(`answer_${i}`).innerHTML = question[`answer_${i}`];
+    }
+}
