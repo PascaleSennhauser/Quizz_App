b5f15b92e8f74d4fd77429f15ef6fb44e9349a9d
PascaleSennhauser <153015334+PascaleSennhauser@users.noreply.github.com>
2024-03-14 11:03:17 +0100

    Added disbled to the next button and when you selected an answer, you can't click on the other answers, anymore

--- script/script.js
+++ script/script.js
@@ -18,9 +18,24 @@ function showQuestion() {
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
     if(selectedQuestionNumber == question['right_answer']) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('nextButton').disabled = false;
+    removeOnClick();
+}
+
+
+function removeOnClick() {
+    for (let i = 1; i <= 4; i++) {
+        let bodyId = document.getElementById(`answer_${i}`);
+        let cardId = bodyId.parentNode;
+        cardId.id = `cardId_${i}`;
+        cardId.onclick = null;
+        cardId.classList.remove('quiz-answer-card');
     }
 }
